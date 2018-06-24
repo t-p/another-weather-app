147a9b115d6d44dfdb7c7d218877fffb6210acaf
Thomas Pfeiffer <email@pfeiffer.pw>
2018-06-24 11:16:48 +0200

    liniting code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import "semantic-ui-css/semantic.min.css";
+import 'semantic-ui-css/semantic.min.css';
 
-import AllDestinations from "./Components/AllDestinations";
-import AddDestination from "./Components/AddDestination";
+import AllDestinations from './Components/AllDestinations';
+import AddDestination from './Components/AddDestination';
 import NewDestinationsSubscription from './Queries/NewDestinationsSubscription';
 
-import AWSAppSyncClient from "aws-appsync";
+import AWSAppSyncClient from 'aws-appsync';
 import { Rehydrated } from 'aws-appsync-react';
-import { AUTH_TYPE } from "aws-appsync/lib/link/auth-link";
+import { AUTH_TYPE } from 'aws-appsync/lib/link/auth-link';
 import { graphql, ApolloProvider, compose } from 'react-apollo';
 import * as AWS from 'aws-sdk';
 import AppSync from './AppSync.js';
@@ -20,15 +20,14 @@ const client = new AWSAppSyncClient({
   url: AppSync.graphqlEndpoint,
   region: AppSync.region,
   auth: {
-      type: AUTH_TYPE.API_KEY,
-      apiKey: AppSync.apiKey,
+    type: AUTH_TYPE.API_KEY,
+    apiKey: AppSync.apiKey
   },
   options: {
     fetchPolicy: 'cache-and-network'
   },
-  disableOffline:false  
+  disableOffline: false
 });
-
 
 const appHeaderStyle = {
   marginBottom: '20px'
@@ -37,33 +36,36 @@ const appHeaderStyle = {
 const headerStyle = {
   color: 'white',
   padding: '10px'
-}
+};
 
 const Home = () => (
-  
   <div>
     <header className="App-header" style={appHeaderStyle}>
       <h2 className="ui center aligned icon header">
-        <img src="https://d1.awsstatic.com/serverless/Lambda%20Resources%20images/sam_acorn_shadow.8915b92ddd48a78d8c05d55ce4b26e472889c573.png" className="App-logo" alt="logo" />
-        <span style={headerStyle}>A Serverless Web App powered by AWS AppSync</span>
+        <img
+          src="https://d1.awsstatic.com/serverless/Lambda%20Resources%20images/sam_acorn_shadow.8915b92ddd48a78d8c05d55ce4b26e472889c573.png"
+          className="App-logo"
+          alt="logo"
+        />
+        <span style={headerStyle}>
+          A Serverless Web App powered by AWS AppSync
+        </span>
       </h2>
     </header>
 
     <div className="ui container">
-
       <h3 className="ui horizontal divider header">
-        <i className="paper plane icon"></i>
-          Travel Destinations and Current Conditions
+        <i className="paper plane icon" />
+        Travel Destinations and Current Conditions
       </h3>
       <AllDestinationsWithData />
       <h4 className="ui horizontal divider header">
-          <i className="tag icon"></i>
-          Create a destination
+        <i className="tag icon" />
+        Create a destination
       </h4>
       <NewDestinationWithData />
     </div>
   </div>
-
 );
 
 const App = () => (
@@ -76,71 +78,80 @@ const App = () => (
 
 const AllDestinationsWithData = compose(
   graphql(AllDestinationsQuery, {
-      options: {
-          fetchPolicy: 'cache-and-network'
-      },
-      props: (props) => ({
-        destinations: props.data.getAllDestinations,
-        // START - NEW PROP :
-        subscribeToDestinations: params => {
-          console.log('Props: ', props);
+    options: {
+      fetchPolicy: 'cache-and-network'
+    },
+    props: props => ({
+      destinations: props.data.getAllDestinations,
+      // START - NEW PROP :
+      subscribeToDestinations: params => {
+        console.log('Props: ', props);
 
-          props.data.subscribeToMore({
-              document: NewDestinationsSubscription,
-              updateQuery: (previousResult, { subscriptionData, variables }) => {
-                // Perform updates on previousResult with subscriptionData
-                console.log(previousResult);
+        props.data.subscribeToMore({
+          document: NewDestinationsSubscription,
+          updateQuery: (previousResult, { subscriptionData, variables }) => {
+            // Perform updates on previousResult with subscriptionData
+            console.log(previousResult);
 
-                var newDestination = subscriptionData.data.newDestination;
-                console.log (newDestination);
+            var newDestination = subscriptionData.data.newDestination;
+            console.log(newDestination);
 
-                const newObj = {};
-                newObj.getAllDestinations = [newDestination, ...previousResult.getAllDestinations];
-                console.log(newObj);
-                
-                return  newObj;
-              }
-              
-              /*(prev, { subscriptionData: {data: {newDestination}} }) => ({
+            const newObj = {};
+            newObj.getAllDestinations = [
+              newDestination,
+              ...previousResult.getAllDestinations
+            ];
+            console.log(newObj);
+
+            return newObj;
+          }
+
+          /*(prev, { subscriptionData: {data: {newDestination}} }) => ({
                 ...prev,
                 getAllDestinations: { getAllDestinations: [newDestination, prev.getAllDestinations.filter(d => d.id != newDestination.id)] , __typename: 'Destinations'}
               })*/
-          });
-        }
-        // END - NEW PROP
-        
-      })
+        });
+      }
+      // END - NEW PROP
+    })
   })
-  )(AllDestinations);
+)(AllDestinations);
 
-  const NewDestinationWithData = graphql(NewDestinationMutation, {
-    props: (props) => ({
-        onAdd: destination => props.mutate({
-            variables: destination,
-            optimisticResponse: () => ({ addDestination: { ...destination, __typename: 'Destination', version: 1 } }),
+const NewDestinationWithData = graphql(NewDestinationMutation, {
+  props: props => ({
+    onAdd: destination =>
+      props.mutate({
+        variables: destination,
+        optimisticResponse: () => ({
+          addDestination: {
+            ...destination,
+            __typename: 'Destination',
+            version: 1
+          }
         })
-    }),
-    options: {
-        //refetchQueries: [{ query: AllDestinationsQuery }],
-        update: (dataProxy, { data: { addDestination } }) => {
-            const query = AllDestinationsQuery;
-            const data = dataProxy.readQuery({ query });
+      })
+  }),
+  options: {
+    //refetchQueries: [{ query: AllDestinationsQuery }],
+    update: (dataProxy, { data: { addDestination } }) => {
+      const query = AllDestinationsQuery;
+      const data = dataProxy.readQuery({ query });
 
-            data.getAllDestinations.destinations.push(addDestination);
+      data.getAllDestinations.destinations.push(addDestination);
 
-            dataProxy.writeQuery({ query, data });
-        },
-        fetchPolicy: 'cache-and-network',
-        disableOffline:false  
-    }
-  })(AddDestination);
+      dataProxy.writeQuery({ query, data });
+    },
+    fetchPolicy: 'cache-and-network',
+    disableOffline: false
+  }
+})(AddDestination);
 
-  const WithProvider = () => (
-    <ApolloProvider client={client}>
-        <Rehydrated>
-            <App />
-        </Rehydrated>
-    </ApolloProvider>
+const WithProvider = () => (
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      <App />
+    </Rehydrated>
+  </ApolloProvider>
 );
 
 export default WithProvider;
